4b8fc2946117a4b988430bb1ed742d721e9b628e
Jose Salazar <jmsalazar.macg@hotmail.com>
2026-04-21 16:37:01 -0600

    home.dart linked to insulin.dart

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import '../../core/theme/app_theme.dart'; 
+import '../../core/theme/app_theme.dart';
+import '../insulin/insulin.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -11,33 +12,53 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       backgroundColor: AppTheme.colorBackground,
       body: SingleChildScrollView(
-        padding: const EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 80), 
+        padding: const EdgeInsets.only(left: 20, right: 20, top: 0, bottom: 80),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 25), 
+            const SizedBox(height: 25),
             Text(
-              "Buenos días, Alex", 
-              style: textTheme.bodyLarge?.copyWith(color: AppTheme.colorTextPrimary), 
+              "Buenos días, Alex",
+              style: textTheme.bodyLarge?.copyWith(
+                color: AppTheme.colorTextPrimary,
+              ),
             ),
             const SizedBox(height: 20),
 
             Row(
               children: [
-                Expanded(child: _buildStatusCard(context, "Glucosa Actual", "110", "mg/dL", Icons.water_drop, AppTheme.colorPrimary)),
+                Expanded(
+                  child: _buildStatusCard(
+                    context,
+                    "Glucosa Actual",
+                    "110",
+                    "mg/dL",
+                    Icons.water_drop,
+                    AppTheme.colorPrimary,
+                  ),
+                ),
                 const SizedBox(width: 15),
-                Expanded(child: _buildStatusCard(context, "Insulina de hoy", "12", "Unidades", Icons.vaccines, AppTheme.colorPrimary)),
+                Expanded(
+                  child: _buildStatusCard(
+                    context,
+                    "Insulina de hoy",
+                    "12",
+                    "Unidades",
+                    Icons.vaccines,
+                    AppTheme.colorPrimary,
+                  ),
+                ),
               ],
             ),
 
             const SizedBox(height: 30),
             Text(
-              "ACCIONES RÁPIDAS", 
+              "ACCIONES RÁPIDAS",
               style: textTheme.bodySmall?.copyWith(
-                color: AppTheme.colorPrimary, 
-                fontWeight: FontWeight.bold, 
-                letterSpacing: 1.2
-              )
+                color: AppTheme.colorPrimary,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1.2,
+              ),
             ),
             const SizedBox(height: 15),
 
@@ -45,66 +66,75 @@ class HomePage extends StatelessWidget {
               children: [
                 Expanded(
                   child: _buildActionButton(
-                    context, 
-                    title: "Insulina", 
-                    icon: Icons.add_circle_outline, 
-                    color: AppTheme.colorPrimary, 
-                    onTap: () {},
+                    context,
+                    title: "Insulina",
+                    icon: Icons.add_circle_outline,
+                    color: AppTheme.colorPrimary,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const InsulinLogPage(),
+                        ),
+                      );
+                    },
                   ),
                 ),
                 const SizedBox(width: 15),
                 Expanded(
                   child: _buildActionButton(
-                    context, 
-                    title: "Glucosa", 
-                    icon: Icons.bloodtype_outlined, 
-                    color: AppTheme.colorError, 
+                    context,
+                    title: "Glucosa",
+                    icon: Icons.bloodtype_outlined,
+                    color: AppTheme.colorError,
                     onTap: () {},
                   ),
                 ),
               ],
             ),
-            
+
             const SizedBox(height: 15),
 
             Center(
               child: SizedBox(
-                width: MediaQuery.of(context).size.width * 0.95, 
+                width: MediaQuery.of(context).size.width * 0.95,
                 child: _buildActionButton(
-                  context, 
-                  title: "Registrar Síntoma", 
-                  icon: Icons.sentiment_dissatisfied_outlined, 
-                  color: AppTheme.colorTertiary, 
+                  context,
+                  title: "Registrar Síntoma",
+                  icon: Icons.sentiment_dissatisfied_outlined,
+                  color: AppTheme.colorTertiary,
                   onTap: () {},
                 ),
               ),
             ),
-            
+
             const SizedBox(height: 30),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "PRÓXIMOS RECORDATORIOS", 
+                  "PRÓXIMOS RECORDATORIOS",
                   style: textTheme.bodySmall?.copyWith(
-                    color: AppTheme.colorPrimary, 
-                    fontWeight: FontWeight.bold, 
-                    letterSpacing: 1.2
-                  )
+                    color: AppTheme.colorPrimary,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1.2,
+                  ),
                 ),
                 TextButton(
-                  onPressed: () {}, 
-                  child: Text("Ver Todo", style: TextStyle(color: AppTheme.colorPrimary))
+                  onPressed: () {},
+                  child: Text(
+                    "Ver Todo",
+                    style: TextStyle(color: AppTheme.colorPrimary),
+                  ),
                 ),
               ],
             ),
-            
 
             _buildReminderCard(
               context,
               icon: Icons.access_time_filled_rounded,
-              iconColor: AppTheme.colorPrimary, 
+              iconColor: AppTheme.colorPrimary,
               iconBgColor: AppTheme.colorSecondary,
               title: "Control Pre-almuerzo",
               description: "Hora de registrar tu glucosa",
@@ -113,13 +143,13 @@ class HomePage extends StatelessWidget {
             _buildReminderCard(
               context,
               icon: Icons.restaurant_menu_rounded,
-              iconColor: AppTheme.colorPrimary, 
+              iconColor: AppTheme.colorPrimary,
               iconBgColor: AppTheme.colorSecondary,
               title: "Bolo Almuerzo",
               description: "Basado en estimación de 45g carbohidratos",
               time: "1:00 PM",
             ),
-            
+
             /*  -- TO DO
             Container(
               padding: const EdgeInsets.all(20),
@@ -142,92 +172,103 @@ class HomePage extends StatelessWidget {
     );
   }
 
-  // Build status 
-Widget _buildStatusCard(BuildContext context, String title, String value, String unit, IconData icon, Color color) {
-  final textTheme = Theme.of(context).textTheme;
+  // Build status
+  Widget _buildStatusCard(
+    BuildContext context,
+    String title,
+    String value,
+    String unit,
+    IconData icon,
+    Color color,
+  ) {
+    final textTheme = Theme.of(context).textTheme;
 
-  return Container(
-    padding: const EdgeInsets.all(16),
-    decoration: BoxDecoration(
-      color: AppTheme.colorBgSecondary,
-      borderRadius: BorderRadius.circular(24),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.black.withOpacity(0.05),
-          blurRadius: 10,
-          offset: const Offset(0, 4),
-        )
-      ],
-    ),
-    child: Column( 
-      crossAxisAlignment: CrossAxisAlignment.start, 
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Icon(icon, color: color, size: 24),
-            const SizedBox(width: 8),
-            Text(
-              title,
-              style: textTheme.bodyLarge?.copyWith(
-                fontSize: 15,
-                fontWeight: FontWeight.w500,
-                color: AppTheme.colorTextPrimary,
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: AppTheme.colorBgSecondary,
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(icon, color: color, size: 24),
+              const SizedBox(width: 8),
+              Text(
+                title,
+                style: textTheme.bodyLarge?.copyWith(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w500,
+                  color: AppTheme.colorTextPrimary,
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
 
-        const SizedBox(height: 16), 
+          const SizedBox(height: 16),
 
-        Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.baseline,
-          textBaseline: TextBaseline.alphabetic,
-          children: [
-            const SizedBox(width: 10),
-            Text(
-              value,
-              style: textTheme.displayLarge?.copyWith(
-                fontSize: 34,
-                fontWeight: FontWeight.bold,
-                color: AppTheme.colorTextPrimary,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.baseline,
+            textBaseline: TextBaseline.alphabetic,
+            children: [
+              const SizedBox(width: 10),
+              Text(
+                value,
+                style: textTheme.displayLarge?.copyWith(
+                  fontSize: 34,
+                  fontWeight: FontWeight.bold,
+                  color: AppTheme.colorTextPrimary,
+                ),
               ),
-            ),
-            const SizedBox(width: 10),
-            Text(
-              unit,
-              style: textTheme.bodySmall?.copyWith(
-                fontSize: 14,
-                color: AppTheme.colorTextPrimary.withOpacity(0.8),
+              const SizedBox(width: 10),
+              Text(
+                unit,
+                style: textTheme.bodySmall?.copyWith(
+                  fontSize: 14,
+                  color: AppTheme.colorTextPrimary.withOpacity(0.8),
+                ),
               ),
-            ),
-          ],
-        ),
-      ],
-    ),
-  );
-}
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
-  // Quick actions
-  Widget _buildActionButton(BuildContext context, {required String title, required IconData icon, required Color color, required VoidCallback onTap}) {
+  Widget _buildActionButton(
+    BuildContext context, {
+    required String title,
+    required IconData icon,
+    required Color color,
+    required VoidCallback onTap,
+  }) {
     return ElevatedButton.icon(
       onPressed: onTap,
       icon: Icon(icon, color: AppTheme.colorTextSecondary, size: 20),
       label: Text(
-        title, 
+        title,
         style: TextStyle(
-          fontFamily: 'NunitoSans', 
-          color: AppTheme.colorTextSecondary, 
-          fontWeight: FontWeight.bold, 
-          fontSize: 15
-        )
+          fontFamily: 'NunitoSans',
+          color: AppTheme.colorTextSecondary,
+          fontWeight: FontWeight.bold,
+          fontSize: 15,
+        ),
       ),
       style: ElevatedButton.styleFrom(
         backgroundColor: color,
-    
+
         padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 10),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
         elevation: 2,
@@ -235,8 +276,8 @@ Widget _buildStatusCard(BuildContext context, String title, String value, String
     );
   }
 
-  // Reminder Card
-  Widget _buildReminderCard(BuildContext context, {
+  Widget _buildReminderCard(
+    BuildContext context, {
     required IconData icon,
     required Color iconColor,
     required Color iconBgColor,
@@ -245,12 +286,12 @@ Widget _buildStatusCard(BuildContext context, String title, String value, String
     required String time,
   }) {
     final textTheme = Theme.of(context).textTheme;
-    
+
     return Container(
-      margin: const EdgeInsets.only(bottom: 12), 
+      margin: const EdgeInsets.only(bottom: 12),
       padding: const EdgeInsets.all(16),
       decoration: BoxDecoration(
-        color: AppTheme.colorBgSecondary, 
+        color: AppTheme.colorBgSecondary,
         borderRadius: BorderRadius.circular(20),
         boxShadow: [
           BoxShadow(
@@ -262,7 +303,6 @@ Widget _buildStatusCard(BuildContext context, String title, String value, String
       ),
       child: Row(
         children: [
-          
           Container(
             padding: const EdgeInsets.all(12),
             decoration: BoxDecoration(
@@ -272,20 +312,19 @@ Widget _buildStatusCard(BuildContext context, String title, String value, String
             child: Icon(icon, color: iconColor, size: 24),
           ),
           const SizedBox(width: 16),
-         
+
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   title,
-                  style: textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
+                  style: textTheme.titleMedium?.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 const SizedBox(height: 4),
-                Text(
-                  description,
-                  style: textTheme.bodySmall,
-                ),
+                Text(description, style: textTheme.bodySmall),
               ],
             ),
           ),
